1db3770f54b20b1fe6d70271eb1c5e8ee28b071e
nik-1207 <nikhil.mohan_cs18@gla.ac.in>
2020-10-14 14:45:27 +0530

    adding profile pic
    Signed-off-by: nik-1207 <nikhil.mohan_cs18@gla.ac.in>

--- app/src/main/java/com/example/chitchat/SettingsActivity.java
+++ app/src/main/java/com/example/chitchat/SettingsActivity.java
@@ -1,16 +1,26 @@
+
 package com.example.chitchat;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -18,18 +28,32 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SettingsActivity extends AppCompatActivity {
 
     private EditText userName, userStatus;
     private String userId;
     private DatabaseReference rootRef;
+    private final int PICK_IMAGE=1;
+    private StorageReference userProfileImageRef;
+    private CircleImageView profileimg;
+
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         userName = findViewById(R.id.user_name);
@@ -38,10 +62,13 @@ public class SettingsActivity extends AppCompatActivity {
         userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
         retrieveUserInfo();
         userName.setVisibility(View.INVISIBLE);
+        userProfileImageRef= FirebaseStorage.getInstance().getReference().child("profile");
+        profileimg=findViewById(R.id.profile_image);
     }
 
     /*Onclick Button Method*/
-    public void updateProfile(View view) {
+    public void updateProfile(View view)
+    {
 
 
         String username = userName.getText().toString();
@@ -73,16 +100,20 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     /* user defined Helper Method */
-    private void retrieveUserInfo() {
+    private void retrieveUserInfo()
+    {
         rootRef.child("Users").child(userId).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if ((snapshot.exists()) && (snapshot.hasChild("username")) && (snapshot.hasChild("image"))) {
+                if ((snapshot.exists()) && (snapshot.hasChild("username")) && snapshot.hasChild("profile")) {
                     String retrieveUserName = snapshot.child("username").getValue().toString();
                     String retrieveUserStatus = snapshot.child("status").getValue().toString();
-                    String retrieveUserImage = snapshot.child("image").getValue().toString();
+                    String retrieveUserImage = snapshot.child("profile").getValue().toString();
+
                     userName.setText(retrieveUserName);
                     userStatus.setText(retrieveUserStatus);
+                    Picasso.get().load(retrieveUserImage).into(profileimg);
+
                 } else if ((snapshot.exists()) && (snapshot.hasChild("username"))) {
                     String retrieveUserName = snapshot.child("username").getValue().toString();
                     String retrieveUserStatus = snapshot.child("status").getValue().toString();
@@ -101,6 +132,53 @@ public class SettingsActivity extends AppCompatActivity {
 
             }
         });
+
+    }
+    public void updateProfilePic(View view)
+    {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery,PICK_IMAGE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==PICK_IMAGE && resultCode==RESULT_OK)
+        {
+           Uri selectedImage = data.getData();
+           CropImage.activity(selectedImage).setGuidelines(CropImageView.Guidelines.ON).setAspectRatio(1,1).start(this);
+        }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE)
+        {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode==RESULT_OK)
+            {
+
+                Uri resultImg=result.getUri();
+
+                final StorageReference filepath=userProfileImageRef.child(userId+".jpg");
+                filepath.putFile(resultImg).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot)
+                    {
+                        filepath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri)
+                            {
+                                String url=uri.toString();
+                                rootRef.child("Users").child(userId).child("profile").setValue(url);
+
+                            }
+                        });
+
+                    }
+                });
+
+
+            }
+        }
 
     }
 }
